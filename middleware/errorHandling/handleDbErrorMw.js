70a5907ab9d7d807ec07844308b29b04040a36cc
G1320 <darnellgreen211@gmail.com>
2024-01-22 21:26:08 +0200

    Latest bulid - Layout improvements

--- middleware/errorHandling/handleDbErrorMw.js
+++ middleware/errorHandling/handleDbErrorMw.js
@@ -1,8 +1,24 @@
+// const ExpressError = require('../../utils/expressError');
+// const { handleDbErrorMsg } = require('../../utils/handleDbErrorMsg');
+
+// const handleDbErrorMw = (err, req, res, next) => {
+//   if (['CastError', 'ValidationError', 'DisconnectedError', 'MongoError'].includes(err.name)) {
+//     err = new ExpressError(handleDbErrorMsg(err), 400);
+//   }
+//   console.error(err.stack);
+//   next(err);
+// };
+
+// module.exports = handleDbErrorMw;
+
 const ExpressError = require('../../utils/expressError');
 const { handleDbErrorMsg } = require('../../utils/handleDbErrorMsg');
 
 const handleDbErrorMw = (err, req, res, next) => {
-  if (['CastError', 'ValidationError', 'DisconnectedError', 'MongoError'].includes(err.name)) {
+  if (err.name === 'CastError') {
+    const errorMessage = `Error when requesting data: ID format is Invalid.`;
+    err = new ExpressError(errorMessage, 400);
+  } else if (['ValidationError', 'DisconnectedError', 'MongoError'].includes(err.name)) {
     err = new ExpressError(handleDbErrorMsg(err), 400);
   }
   console.error(err.stack);
